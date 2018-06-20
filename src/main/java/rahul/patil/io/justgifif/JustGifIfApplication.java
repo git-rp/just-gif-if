2fb0018d244f5983ee7b1bd0737bd99326ad0d10
rapatil <rapatil@ccrn.com>
2018-06-20 15:54:35 +0530

    first commit

---
+++ src/main/java/rahul/patil/io/justgifif/JustGifIfApplication.java
@@ -0,0 +1,12 @@
+package rahul.patil.io.justgifif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGifIfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGifIfApplication.class, args);
+	}
+}
